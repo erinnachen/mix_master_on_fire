0f9bdc36f6f9b558c06ded7aef7fa4dfc65ccb5a
Erinna Chen <erinna.chen@gmail.com>
2016-05-31 16:23:03 -0600

    Add mix_test_watch to autorun tests

--- mix.exs
+++ mix.exs
@@ -44,6 +44,7 @@ defmodule MixMaster.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
+     {:mix_test_watch, "~> 0.2", only: :dev},
      {:hound, "~> 1.0", only: [:dev, :test]}]
   end
 
